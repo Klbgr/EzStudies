6d848a0fd3d7d557a936e287c40455a45732503b
Klbgr <qiuantoine@free.fr>
2021-11-15 21:58:58 +0100

    Updated UI

--- app/src/main/java/com/ezstudies/app/Overview.java
+++ app/src/main/java/com/ezstudies/app/Overview.java
@@ -1,6 +1,8 @@
 package com.ezstudies.app;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,5 +12,17 @@ public class Overview extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.overview_layout);
+    }
+
+    public void agenda(View view){
+        startActivity(new Intent(this, Agenda.class));
+    }
+
+    public void homework(View view){
+        startActivity(new Intent(this, Homework.class));
+    }
+
+    public void settings(View view){
+        startActivity(new Intent(this, Settings.class));
     }
 }
--- app/src/main/java/com/ezstudies/app/Welcome.java
+++ app/src/main/java/com/ezstudies/app/Welcome.java
@@ -1,18 +1,37 @@
 package com.ezstudies.app;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Welcome extends AppCompatActivity {
+    private SharedPreferences sharedPreferences;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.welcome_layout);
 
-        Intent intent = new Intent(this, CelcatParser.class);
-        startActivity(intent);
+        sharedPreferences = this.getSharedPreferences("firstTime", Context.MODE_PRIVATE);
+        Boolean firstTime = sharedPreferences.getBoolean("firstTime", true);
+
+        if(firstTime){
+            setContentView(R.layout.welcome_layout);
+        }
+        else{
+            finish();
+            startActivity(new Intent(this, Overview.class));
+        }
+    }
+
+    public void enter(View view){
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putBoolean("firstTime", false);
+        editor.commit();
+        finish();
+        startActivity(new Intent(this, Overview.class));
     }
 }
