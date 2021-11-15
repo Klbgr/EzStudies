38fd81e8144565390875c72eeaac8e7c899af50a
Klbgr <qiuantoine@free.fr>
2021-11-15 22:57:00 +0100

    Updated UI

--- app/src/main/java/com/ezstudies/app/Agenda.java
+++ app/src/main/java/com/ezstudies/app/Agenda.java
@@ -1,6 +1,8 @@
 package com.ezstudies.app;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,5 +11,9 @@ public class Agenda extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.agenda_layout);
+    }
+
+    public void celcat(View view){
+        startActivity(new Intent(this, CelcatParser.class));
     }
 }
